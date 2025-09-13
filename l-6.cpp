b796ef4496942de7e61d237eb66e0ecef9ee9d13
Annie <annmora21@gmail.com>
2025-09-13 13:09:23 -0700

    v-0.3

--- l-6.cpp
+++ l-6.cpp
@@ -12,24 +12,30 @@ double sumArray(double*);
 main()
 {
     const int ARRAYSIZE = 5;
-    double numbers[ARRAYSIZE];
+
+    double *array = nullptr; // Pointer to hold the address of the dynamic array
+    array = new double[ARRAYSIZE]; // Dynamically allocates memory for the array
 
     enterArrayData();
     outputArrayData();
-    sumArray();
+
+    double sum = sumArray(array);
+    cout << "Sum of Values: " << sum << endl;
+
+    delete[] array;  // Free the dynamically allocated memory
 
     return 0;
 }
 
 // Receives a dynamic double array and populates it with values from user input.
-void enterArrayData(double*);
+void enterArrayData(double* array);
 {
     const int ARRAYSIZE = 5;
-    double numbers[ARRAYSIZE];
 
     cout << "Data entry for the array: " << endl;
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < ARRAYSIZE; i++)
     {
-        cin >> numbers[i];
+        cout << "     > Element " << (i + 1) << ": ";
+        cin >> array[i];
     }
 }
